8a646f5b6aa863afa378e36e2860dd89ce4dd5aa
plchang <149220351+plchang@users.noreply.github.com>
2024-01-17 20:24:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='st7789py_mpy',
-    packages=['st7789py_mpy'],
-    package_dir={'st7789py_mpy'},
-    ...
-    entry_points={ 'console_scripts': ['st7789py_mpy =__main__:main' ] }
-setup(
-    name='st7789py_mpy',
     version='0.1',
     packages=find_packages(),
     package_dir={'st7789py_mpy'},
